6c75d3ee8e91869943c2e1895ddb9a1357ea30ff
vinimacar <vinicius.machado.carvalho@educacao.mg.gov.br>
2026-01-04 15:52:05 -0300

    Fix routing for GitHub Pages

--- ccb-organiza-main/src/App.tsx
+++ ccb-organiza-main/src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/congregacoes" element={<Congregacoes />} />
